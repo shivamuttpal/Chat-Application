f3bd389a687569936a5daa5ce12139bdd958863e
shivam uttpal jha <shivamuttpal@gmail.com>
2024-02-15 22:32:07 +0530

    done

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
-// index.js
-const io = require('socket.io')(8000);
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const users = {};
 
+// Socket.io events
 io.on('connection', (socket) => {
   socket.on('new-user-joined', (name) => {
     users[socket.id] = name;
@@ -16,4 +19,15 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('left', users[socket.id]);
     delete users[socket.id];
   });
+});
+
+// HTTP server for handling GET requests
+app.get('/hello', (req, res) => {
+  res.send('Hello, this is my chat backend');
+});
+
+// Start the HTTP server
+const PORT = 8000;
+http.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
